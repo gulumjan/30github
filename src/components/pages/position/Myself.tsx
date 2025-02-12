f833033b6166f5dfc8fd41067bc94a0e1e47d90f
gulumjan <zakirovagulumjan53@gmail.com>
2025-02-12 23:47:21 +0600

    updating

--- src/components/pages/position/Myself.tsx
+++ src/components/pages/position/Myself.tsx
@@ -7,10 +7,20 @@ const Myself: FC = () => {
       <div className="container">
         <div className={scss.content}>
           <div className={scss.text}>
-            <p className={scss.intro}>I am Gulumzhan</p>
+            <h1 className={scss.intro}>Zakirova Gulumzhan</h1>
             <h1>
               <span>Frontend Developer</span>
             </h1>
+            <p>
+              {" "}
+              laboriosam soluta repudiandae. Quis ducimus illo aliquam magni
+              distinctio rem commodi cum quaerat minus? Exercitationem,
+              accusamus. Officia error distinctio rem minima cum voluptatem
+              quidem. Aperiam ex iusto aut vero! Iste ullam ex quam quas
+              voluptas labore mollitia ratione dolorum amet corporis inventore
+              eum soluta blanditiis officia aliquid aut magni iusto veritatis
+              nihil, dicta voluptates.
+            </p>
           </div>
         </div>
       </div>
