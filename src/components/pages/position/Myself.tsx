091e8ac291731d77f7f815e4c4799d2700837042
gulumjan <zakirovagulumjan53@gmail.com>
2025-02-15 17:13:13 +0600

    background is changed

--- src/components/pages/position/Myself.tsx
+++ src/components/pages/position/Myself.tsx
@@ -5,23 +5,7 @@ const Myself: FC = () => {
   return (
     <section className={scss.Myself}>
       <div className="container">
-        <div className={scss.content}>
-          <div className={scss.text}>
-            <h1 className={scss.intro}>Zakirova Gulumzhan</h1>
-            <h1>
-              <span>Frontend Developer</span>
-            </h1>
-          </div>
-          <p>
-            {" "}
-            laboriosam soluta repudiandae. Quis ducimus illo aliquam magni
-            distinctio rem commodi cum quaerat minus? Exercitationem, accusamus.
-            Officia error distinctio rem minima cum voluptatem quidem. Aperiam
-            ex iusto aut vero! Iste ullam ex quam quas voluptas labore mollitia
-            ratione dolorum amet corporis inventore eum soluta blanditiis
-            officia aliquid aut magni iusto veritatis nihil, dicta voluptates.
-          </p>
-        </div>
+        <div className={scss.content}></div>
       </div>
     </section>
   );
