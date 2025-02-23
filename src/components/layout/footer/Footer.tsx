42bb4d3ba975a1a157fdbc1b954151e9d3443edc
gulumjan <zakirovagulumjan53@gmail.com>
2025-02-23 21:53:56 +0600

    added footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -5,7 +5,14 @@ const Footer: FC = () => {
   return (
     <footer className={scss.Footer}>
       <div className="container">
-        <div className={scss.content}>Footer</div>
+        <div className={scss.content}>
+          Footer
+          <nav>
+            <a href="#">Contact</a>
+            <a href="#">About</a>
+            <a href="">Author</a>
+          </nav>
+        </div>
       </div>
     </footer>
   );
